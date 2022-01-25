1f4ef219a6c5a65a43608817f6d82c031def36d5
Matthew Celnik <matthew.celnik@dnv.com>
2022-01-25 16:18:04 +0000

    Added some data fabric examples

--- examples/deep_search.py
+++ examples/deep_search.py
@@ -1,28 +1,48 @@
+"""  Veracity SDK Example: Run Deep Search using a client credential.
+"""
 import os
+import azure.identity
+import azure.keyvault.secrets
 import requests
-from veracity_platform.identity import InteractiveBrowserCredential
+from veracity_platform.identity import ClientSecretCredential
 
-CLIENT_ID = os.environ['EXAMPLE_VERACITY_CLIENT_ID']
-CLIENT_SECRET = os.environ['EXAMPLE_VERACITY_CLIENT_SECRET']
-SUBSCRIPTION = os.environ['EXAMPLE_VERACITY_SUBSCRIPTION_KEY']
-REDIRECT_URI = "http://localhost/login"
 
-cred = InteractiveBrowserCredential(
-    client_id=CLIENT_ID,
-    redirect_uri=REDIRECT_URI,
-    client_secret=CLIENT_SECRET,
-)
+# For convenience I have put my app credentials in an Azure Key Vault.  Will
+# fall back on environment variables though.
+try:
+    kvurl = os.environ['TEST_KEYVAULT_URL']
+    kvcred = azure.identity.DefaultAzureCredential()
+    kvclient = azure.keyvault.secrets.SecretClient(kvurl, kvcred)
+    CLIENT_ID = kvclient.get_secret('TestApp-ID').value
+    CLIENT_SECRET = kvclient.get_secret('TestApp-Secret').value
+    SUBSCRIPTION = kvclient.get_secret('TestApp-Sub').value
+except KeyError:
+    CLIENT_ID = os.environ['TESTAPP_CLIENT_ID']
+    CLIENT_SECRET = os.environ['TESTAPP_CLIENT_SECRET']
+    SUBSCRIPTION = os.environ['TESTAPP_SUBSCRIPTION_KEY']
 
-token = cred.get_token(scopes=['https://dnvglb2cprod.onmicrosoft.com/dfba9693-546d-4300-bcd7-d8d525bdff38'])
-print(token)
-access_token = token['access_token']
 
-uri = "https://api.veracity.com/veracity/datafabric/search/api/v1/me/Services"
-response = requests.get(
-    uri,
-    headers={'Ocp-Apim-Subscription-Key': SUBSCRIPTION, 'Authorization': f'Bearer {access_token}'},
-)
+def main():
+    cred = ClientSecretCredential(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+    )
+    token = cred.get_token(scopes=['veracity_datafabric'])
+    print(token)
+    access_token = token['access_token']
 
-print(response.status_code)
-print(response.text)
+    uri = "https://api.veracity.com/veracity/datafabric/search/api/v1/me/Services"
+    response = requests.get(
+        uri,
+        headers={
+            'Ocp-Apim-Subscription-Key': SUBSCRIPTION,
+            'Authorization': f'Bearer {access_token}',
+        },
+    )
 
+    print(response.status_code)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    main()
