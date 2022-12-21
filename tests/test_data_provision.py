0b955f2dbbb7056795472d8d5fb984fabf1fa9ef
Matthew Celnik <matthew.celnik@dnv.com>
2022-12-21 14:54:55 +0000

    Added basic tests for provisioning endpoints

--- tests/test_data_provision.py
+++ tests/test_data_provision.py
@@ -48,8 +48,7 @@ class TestProvisionAPI(object):
 
     @pytest.mark.asyncio
     async def test_create_container(self, api):
-        """ Creating a new container has no exceptions.
-        """
+        """Creating a new container has no exceptions."""
         with patch_response(api.session, "post", 202, text="MOCK_GUID") as mockpost:
             data = await api.create_container(
                 "mycontainer",
@@ -72,3 +71,103 @@ class TestProvisionAPI(object):
                 "https://api.veracity.com/veracity/datafabric/provisioning/api/1/container", json=expected_body
             )
             assert data == "MOCK_GUID"
+
+    @pytest.mark.asyncio
+    async def test_copy_container(self, api):
+        """Copying a container has no exceptions."""
+        with patch_response(api.session, "post", 202, text="") as mockpost:
+            await api.copy_container(
+                "mycontainer",
+                "myaccess",
+                "newcontainer",
+                "New Container",
+                description="My copied container",
+                tags=["my", "container"],
+            )
+
+            expected_body = {
+                "sourceResourceId": "mycontainer",
+                "copyResourceShortName": "newcontainer",
+                "copyResourceMayContainPersonalData": False,
+                "copyResourceTitle": "New Container",
+                "copyResourceDescription": "My copied container",
+                "copyResourceIcon": {"id": "Automatic_Information_Display", "backgroundColor": "#5594aa"},
+                "copyResourceTags": [{"title": "my", "type": "tag"}, {"title": "container", "type": "tag"}],
+            }
+            mockpost.assert_called_with(
+                "https://api.veracity.com/veracity/datafabric/provisioning/api/1/container/copycontainer",
+                json=expected_body,
+                params={"accessId": "myaccess"},
+            )
+
+    @pytest.mark.asyncio
+    async def test_delete_container(self, api):
+        """Deleting a container has no exceptions."""
+        with patch_response(api.session, "delete", 202, text="") as mockdelete:
+            await api.delete_container("mycontainer")
+            mockdelete.assert_called_with(
+                "https://api.veracity.com/veracity/datafabric/provisioning/api/1/container/mycontainer"
+            )
+
+    @pytest.mark.asyncio
+    async def test_create_event_subscription(self, api):
+        """Creating a new event subscription has no exceptions."""
+        with patch_response(api.session, "post", 202, text="") as mockpost:
+            await api.create_event_subscription(
+                "mysub",
+                "AccessShare",
+                "https://mycallback",
+            )
+
+            expected_body = {
+                "subscriptionName": "mysub",
+                "callback": "https://mycallback",
+                "topic": "AccessShare",
+                "regions": ["westeurope"],
+            }
+            mockpost.assert_called_with(
+                "https://api.veracity.com/veracity/datafabric/provisioning/api/1/container/SubscribeToCustomEvents",
+                json=expected_body,
+            )
+
+    @pytest.mark.asyncio
+    async def test_delete_event_subscription(self, api):
+        """Deleting a subscription has no exceptions."""
+        with patch_response(api.session, "delete", 202, text="") as mockdelete:
+            await api.delete_event_subscription("mysub")
+            mockdelete.assert_called_with(
+                "https://api.veracity.com/veracity/datafabric/provisioning/api/1/container/SubscribeToCustomEvents",
+                json={"subscriptionName": "mysub"},
+            )
+
+    @pytest.mark.asyncio
+    async def test_create_blob_change_subscription(self, api):
+        """Creating a new event subscription has no exceptions."""
+        with patch_response(api.session, "post", 202, text="") as mockpost:
+            await api.create_blob_change_subscription(
+                "mysub",
+                "mycontainer",
+                ["BlobUpserted"],
+                "https://mycallback",
+            )
+
+            expected_body = {
+                "subscriptionName": "mysub",
+                "containerId": "mycontainer",
+                "subscriptionTypes": ["BlobUpserted"],
+                "callback": "https://mycallback",
+            }
+            mockpost.assert_called_with(
+                "https://api.veracity.com/veracity/datafabric/provisioning/api/1/container/SubscribeToBlobContainerEvents",
+                json=expected_body,
+            )
+
+    @pytest.mark.asyncio
+    async def test_delete_blob_change_subscription(self, api):
+        """Deleting a subscription has no exceptions."""
+        with patch_response(api.session, "delete", 202, text="") as mockdelete:
+            await api.delete_blob_change_subscription("mysub", "mycontainer")
+            mockdelete.assert_called_with(
+                "https://api.veracity.com/veracity/datafabric/provisioning/api/1/container/SubscribeToBlobContainerEvents",
+                json={"subscriptionName": "mysub", "containerId": "mycontainer"},
+            )
